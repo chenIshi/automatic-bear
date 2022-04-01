8314159853568de4f375777a59edc14ceadf9870
chenishi <chen.ishi@gmail.com>
2022-04-01 15:07:11 +0800

    Init draft structure for tunneler script

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # automatic-bear
-A simple script to automatically maintain a reverse ssh tunnel, re-establishing the ssh connection if failed.
+A simple script to automatically maintain a reverse SSH tunnel, re-establishing the SSH connection if failed.
+
+## Motivation
+
+In order to access servers within intranets with a public address, we currently adopt 
+reverse SSH as work-around. However, the SSH connection session brokes easily 
+due to various unexpected network events.
+
+### Unreliable Tunneler
+The liveness of the *tunneler*, a public-accessible VM on ali-cloud, is rather unreliable.
+Sometimes the intranet servers can't reach the *tunneler* to maintain the reverse 
+SSH session, leading to a connection failure.
+
+### **Autossh** Not Working
+
+## Objective
+
+### Fault-tolerant SSH Session
+
+Rely on periodical SSH re-establishment to resist potential *tunneler* failure.
+
+### QoS Evaluation
+
+Help troubleshoot the root cause of SSH disconnection.
+
+### Anomaly Reports
+
+Even with automatic session recovery, the reverse SSH connection could still fails 
+due to other unexpected reasons. As a result, the system should notify the operator 
+once it can't recover the reverse SSH session.
+
+## Quick Start
---
+++ tunneler.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# configure
+probe_timeout_in_second=300
+tunneler_public_address="101.201.234.77"
+tunneler_fwd_ports=(1234 1237)
+tunneler_dest_size=${#tunneler_fwd_ports[@]}
+
+err_log_name="reverseSSH_log"
+
+declare -A tunneler_pids
+
+declare -i tunneler_failure_times
+failure_anomlies_threshold=20
+
+declare -i connection_retrial_times
+connection_maximum_failure=20
+
+# reset(kill) old reverse SSH sessions
+# TODO: only reset specific reverse SSH sessions created by this script
+kill $(ps aux | grep 'ssh -fCNR' | awk '{print $2}')
+
+# init reverse SSH sessions
+for dest in $(seq 0 $((${tunneler_dest_size}-1)))
+do 
+  ssh -fCNR ${tunneler_public_address}:${tunneler_fwd_ports[$dest]}:localhost:22 root@${tunneler_public_address} -o ServerAliveInterval=60 & 
+  # record the tunneler pid for latter liveness check
+  tunneler_pids[${dest}]=$!
+done 
+
+while true
+do
+  sleep ${probe_timeout_in_second}
+  for idx in ${!tunneler_pids[@]}
+  do 
+    # check if the tunneler process still persists
+    if ps -p ${tunneler_pids[${idx}]} > /dev/null
+    then
+      # reset connection trial count
+      connection_retrial_times=0
+      tunneler_failure_times=0
+    else
+      # retry for a limited times
+      # TODO: a budget-based retrial scheme that notify 
+      # network operators once run out of retrial budgets 
+      # currently as a work-around, we can temporarily 
+      # try once per probing interval, considering that 
+      # ali-cloud server failure could last for a while 
+      # (so that there is no point retrying within a probing interval) 
+      
+      # first using ping to check network connection
+      if ping -q -c 1 -W 1 ${tunneler_public_address} > /dev/null
+      then
+        if [ ${connection_retrial_times} -lt ${connection_maximum_failure} ]
+        then
+          ssh -fCNR ${tunneler_public_address}:${tunneler_fwd_ports[$idx]}:localhost:22 root@${tunneler_public_address} -o ServerAliveInterval=60 & 
+          # record the tunneler pid for latter liveness check
+          tunneler_pids[${idx}]=$!
+          tunneler_failure_times=0
+          connection_retrial_times+=1
+        else
+          errDate=`date`
+          echo "20 sequential reconnection trials failed at " ${errDate} 1> ${err_log_name}
+          exit 1
+        fi
+      else
+        # ali-cloud server network failure
+        # TODO:accmulate the network failure and notify 
+        # operators on successive failures
+        tunneler_failure_times+=1
+        if [ ${tunneler_failure_times} -gt ${failure_anomlies_threshold} ]
+        then
+          errDate=`date`
+          echo "20 sequential connection failures at " ${errDate} 1> ${err_log_name}
+          exit 1
+        fi
+      fi
+    fi
+  done
+  
+done
